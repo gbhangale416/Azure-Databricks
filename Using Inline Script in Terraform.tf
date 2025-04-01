ebeec8cc528e9b8942b4bf259d321a3814ef6565
Girish Bhangale <gbhangale416@gmail.com>
2025-04-01 11:16:05 +0530

    Update Using Inline Script in Terraform.tf

--- Using Inline Script in Terraform.tf
+++ Using Inline Script in Terraform.tf
@@ -26,3 +26,34 @@ EOF
     }
   }
 }
+
+
+
+
+
+locals {
+  packages = [
+    "package-one",
+    "package-two",
+    "internal-lib",
+    "another-tool"
+  ]
+  trusted_hosts = "--trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org"
+}
+
+resource "databricks_cluster" "example_cluster" {
+  cluster_name            = "my-databricks-cluster"
+  spark_version           = "14.1.x-scala2.12"
+  node_type_id            = "Standard_DS3_v2"
+  autotermination_minutes = 60
+  num_workers             = 2
+
+  init_scripts {
+    inline = <<EOT
+#!/bin/bash
+$(join("\n", [
+  for package in local.packages : "pip install ${local.trusted_hosts} ${package}"
+]))
+EOT
+  }
+}
