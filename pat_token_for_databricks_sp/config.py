95e1bac33cc9ded0e271edfdc6581ec6be3975b1
Girish Bhangale <gbhangale416@gmail.com>
2026-09-08 12:06:17 +0530

    Update config.py

--- pat_token_for_databricks_sp/config.py
+++ pat_token_for_databricks_sp/config.py
@@ -1,32 +1,76 @@
 # ============================================================
-# Databricks Workspace Configuration
+# Databricks Workspace URL
+# Same URL for all departments in an environment
 # ============================================================
 
 WORKSPACES = {
     "dev": "https://<DEV-WORKSPACE>.azuredatabricks.net",
-    "test": "https://<TST-WORKSPACE>.azuredatabricks.net",
+    "test": "https://adb-4433143851805380.0.azuredatabricks.net",
     "prod": "https://<PROD-WORKSPACE>.azuredatabricks.net"
 }
 
 
 # ============================================================
-# Department Client Credentials
+# Client Credentials
+# Different credentials for each Environment + Department
 # ============================================================
 
 CLIENT_CREDENTIALS = {
-    "BIA": {
-        "client_id": "<BIA_CLIENT_ID>",
-        "client_secret": "<BIA_CLIENT_SECRET>"
+
+    "dev": {
+
+        "BIA": {
+            "client_id": "<DEV_BIA_CLIENT_ID>",
+            "client_secret": "<DEV_BIA_CLIENT_SECRET>"
+        },
+
+        "CMO": {
+            "client_id": "<DEV_CMO_CLIENT_ID>",
+            "client_secret": "<DEV_CMO_CLIENT_SECRET>"
+        },
+
+        "IDE": {
+            "client_id": "<DEV_IDE_CLIENT_ID>",
+            "client_secret": "<DEV_IDE_CLIENT_SECRET>"
+        }
     },
 
-    "CMO": {
-        "client_id": "<CMO_CLIENT_ID>",
-        "client_secret": "<CMO_CLIENT_SECRET>"
+
+    "test": {
+
+        "BIA": {
+            "client_id": "<TEST_BIA_CLIENT_ID>",
+            "client_secret": "<TEST_BIA_CLIENT_SECRET>"
+        },
+
+        "CMO": {
+            "client_id": "<TEST_CMO_CLIENT_ID>",
+            "client_secret": "<TEST_CMO_CLIENT_SECRET>"
+        },
+
+        "IDE": {
+            "client_id": "<TEST_IDE_CLIENT_ID>",
+            "client_secret": "<TEST_IDE_CLIENT_SECRET>"
+        }
     },
 
-    "IDE": {
-        "client_id": "<IDE_CLIENT_ID>",
-        "client_secret": "<IDE_CLIENT_SECRET>"
+
+    "prod": {
+
+        "BIA": {
+            "client_id": "<PROD_BIA_CLIENT_ID>",
+            "client_secret": "<PROD_BIA_CLIENT_SECRET>"
+        },
+
+        "CMO": {
+            "client_id": "<PROD_CMO_CLIENT_ID>",
+            "client_secret": "<PROD_CMO_CLIENT_SECRET>"
+        },
+
+        "IDE": {
+            "client_id": "<PROD_IDE_CLIENT_ID>",
+            "client_secret": "<PROD_IDE_CLIENT_SECRET>"
+        }
     }
 }
 
@@ -35,4 +79,4 @@ CLIENT_CREDENTIALS = {
 # PAT Configuration
 # ============================================================
 
-LIFETIME_SECONDS = 31536000  # 1 year
+LIFETIME_SECONDS = 31536000  # 365 days
